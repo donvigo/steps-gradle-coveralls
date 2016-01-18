83e6ca708c9ffc4924108a8635d3c3dd32581d40
vgaidarji <veaceslav.gaidarji@yopeso.com>
2016-01-18 12:57:12 +0200

    fix step configuration.

--- step.sh
+++ step.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 if [ -z "${gradlew_file_path}" ] ; then
-	printf "\e[31gradlew_file_path was not defined\e[0m\n"
+	printf "\e[31mgradlew_file_path was not defined\e[0m\n"
     exit 1
 fi
 
@@ -10,10 +10,17 @@ if [ ! -f "${gradlew_file_path}" ] ; then
     exit 1
 fi
 
-if [ -z “${coveralls_task}” ] ; then
+if [ -z "${coveralls_task}" ] ; then
     printf "\e[31mcoveralls_task was not defined\e[0m\n"
     exit 1
 fi
+
+if [ -z "${coveralls_repo_token}" ] ; then
+    printf "\e[31mcoveralls_repo_token was not defined.\e[0m\n"
+    exit 1
+fi
+
+export COVERALLS_REPO_TOKEN=${coveralls_repo_token}
 
 echo "$" ${gradlew_file_path} ${coveralls_task}
 ${gradlew_file_path} ${coveralls_task}
